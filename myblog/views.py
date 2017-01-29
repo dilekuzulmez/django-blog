4ab4af71d71d4913fdb5a61b1dd0795bf278f36a
dilekuzulmez <dilekuzulmez@gmail.com>
2017-01-30 01:18:14 +0300

    Added view for update and create in mysite :page_facing_up:

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +14,30 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'myblog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.yazar = request.user
+            post.yayinlanma_tarihi = timezone.now()
+            post.save()
+            return redirect("/")
+    else:
+        form = PostForm()
+    return render(request, 'myblog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.yazar = request.user
+            post.yayinlanma_tarihi = timezone.now()
+            post.save()
+            return redirect('/post/'+pk, pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'myblog/post_edit.html', {'form': form})
